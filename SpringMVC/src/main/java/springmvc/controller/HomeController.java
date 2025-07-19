852da5ef9b57836c806b535addf44aee752a0460
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2025-07-19 17:34:32 +0530

    Learnt and Implemented sending data from Controller to View(jsp) from request mapping using Model addAttribute() and getAttribute() methods

--- SpringMVC/src/main/java/springmvc/controller/HomeController.java
+++ SpringMVC/src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,33 @@
 package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("Hello, inside Home request url !!!");
+        /* Adding the dynamic model data to the view (jsp) through the controller */
+        model.addAttribute("name","Bruce Wayne");
+        model.addAttribute("id",001);
+
+        List<String> paramVeers = new ArrayList<>();
+        paramVeers.add("Major Somnath Sharma");
+        paramVeers.add("Naik Jadunath Singh");
+        paramVeers.add("Flying Officer Nirmal Jit Singh Sekhon");
+        paramVeers.add("Captain Manoj Kumat Pandey");
+        paramVeers.add("Captain Vikram Batra");
+        paramVeers.add("Naib Subedar Bana Singh");
+        paramVeers.add("Grenadier Yogendra Singh Yadav");
+
+        model.addAttribute("pvc",paramVeers);
+
         return "index";
     }
 
--- SpringMVC/src/main/webapp/WEB-INF/views/index.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 pageEncoding="ISO-8859-1"%>
+<%@ page import="java.util.List" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,5 +11,27 @@ pageEncoding="ISO-8859-1"%>
     <h1>This is my Home Page !!! </h1>
     <h1>Called from Home Controller </h1>
     <h1>url /home </h1>
+
+    <%
+     String name = (String)request.getAttribute("name");
+     Integer id = (Integer)request.getAttribute("id");
+     List<String> pvc = (List<String>)request.getAttribute("pvc");
+     %>
+
+     <h1>Name is <%=name%></h1>
+     <h1>Id is <%=id%></h1>
+     <h1>
+     Param Veers :-- <%
+     for(String s: pvc){
+     //out.println(s);
+     %>
+     <h1><%=s%></h1>
+     <%
+     }
+
+     %>
+
+
+     </h1>
 </body>
 </html>
