05885b1aea5a9c4c156f319c47ca5a2e96e7cd50
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2025-07-25 12:30:57 +0530

    Learnt about path variable and @PathVariable annotations to read path variable from uri

--- SpringMVCSearch/SpringMVCSearch/src/main/java/com/springMvcSearch/SearchController.java
+++ SpringMVCSearch/SpringMVCSearch/src/main/java/com/springMvcSearch/SearchController.java
@@ -1,12 +1,20 @@
 package com.springMvcSearch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class SearchController {
+
+    @RequestMapping("user/{userId}/{userName}")
+    public String getUserDetail(@PathVariable("userId") int userId, @PathVariable("userName") String userName){
+        System.out.println("UserID:- "+ userId);
+        System.out.println("UserName:- "+ userName);
+        return "home";
+    }
 
     @RequestMapping("/home")
     public String home(){
