483f8bc4588a1cd8a16efb94b6d6041977c8fb4f
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2026-01-26 23:22:01 +0530

    Some changes to signup.html

--- LearningServletsJSP/src/main/java/com/deepsank/SignUp/Register.java
+++ LearningServletsJSP/src/main/java/com/deepsank/SignUp/Register.java
@@ -25,25 +25,16 @@ public class Register extends HttpServlet {
     private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException{
         resp.setContentType("text/html;charset=UTF-8");
         try(PrintWriter out = resp.getWriter()){
-
             String name = req.getParameter("name");
             String password = req.getParameter("password");
             String email = req.getParameter("email");
             Part part = req.getPart("image");
             String fileName= part.getSubmittedFileName();
-
             //Store in DB:---
-            // Path to your .db file
-            String url = "jdbc:sqlite:D:/LearningProjects/SQLLite/Servlet_JSP_learnig.db";
-            /*Explicitly Register the Driver
-            While modern Java (JDBC 4.0+) loads drivers automatically,
-            web environments sometimes fail to auto-discover drivers in
-            WEB-INF/lib. Force the loading by adding this line before you call
-            DriverManager.getConnection
-            * */
+            String url = "jdbc:sqlite:D:/LearningProjects/SQLLite/Servlet_JSP_learnig.db";  // Path to your .db file
+            /* Explicitly Register the Driver While modern Java (JDBC 4.0+) loads drivers automatically, web environments sometimes fail to auto-discover
+            drivers in WEB-INF/lib. Force the loading by adding this line before you call DriverManager.getConnection * */
             Class.forName("org.sqlite.JDBC");
-
-
             try (Connection conn = DriverManager.getConnection(url)) {
                 if (conn != null) {
                     System.out.println("Connection to SQLite established!");
@@ -56,13 +47,11 @@ public class Register extends HttpServlet {
                 pstmt.setString(3,email);
                 pstmt.setString(4,fileName);
                 pstmt.executeUpdate();
-
                 //upload at path
                 InputStream is = part.getInputStream();
                 byte[] data = new byte[is.available()];
                 is.read(data);
                 String filePath = req.getRealPath("/")+"images"+ File.separator+fileName;
-
                 FileOutputStream fos = new FileOutputStream(filePath);
                 fos.write(data);
                 fos.close();
