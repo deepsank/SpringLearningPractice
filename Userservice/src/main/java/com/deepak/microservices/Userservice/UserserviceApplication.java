176af94cac4ea15d81cf4dcfc6ea4b5c0baf8b5e
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2026-02-02 23:52:20 +0530

    Communicated between UserService and HotelService using RestTemplate client

--- Userservice/src/main/java/com/deepak/microservices/Userservice/UserserviceApplication.java
+++ Userservice/src/main/java/com/deepak/microservices/Userservice/UserserviceApplication.java
@@ -2,10 +2,16 @@ package com.deepak.microservices.Userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
+//	@Bean
+//	public RestTemplate restTemplate(){
+//		return new RestTemplate();
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
--- Userservice/src/main/java/com/deepak/microservices/Userservice/services/impl/UserServiceImpl.java
+++ Userservice/src/main/java/com/deepak/microservices/Userservice/services/impl/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package com.deepak.microservices.Userservice.services.impl;
 
+import com.deepak.microservices.Userservice.entities.Rating;
 import com.deepak.microservices.Userservice.entities.User;
 import com.deepak.microservices.Userservice.exceptions.ResourceNotFoundException;
 import com.deepak.microservices.Userservice.repositories.UserRepository;
 import com.deepak.microservices.Userservice.services.UserService;
+import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +21,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @Override
     public User saveUser(User user) {
         //Generate unique userId for each user
@@ -30,6 +41,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given ID not found on the server!!! :-- ID:-- "+userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given ID not found on the server!!! :-- ID:-- " + userId));
+        //http://localhost:8083/ratings/users/26b771df-efaf-4937-a3ce-0bc2f4f42255
+        ArrayList<Rating> rating = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        logger.info("{} ",rating);
+        user.setRatings(rating);
+        return user;
     }
 }
