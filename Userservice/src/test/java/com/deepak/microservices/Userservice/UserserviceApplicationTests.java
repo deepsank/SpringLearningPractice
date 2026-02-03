f1dd8e0b4c85c6ffb44f7f963c0d75d9e8121f99
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2026-02-04 01:28:16 +0530

    Implemented Interservices communication using FeignClient and implemented POST method

--- Userservice/src/test/java/com/deepak/microservices/Userservice/UserserviceApplicationTests.java
+++ Userservice/src/test/java/com/deepak/microservices/Userservice/UserserviceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.deepak.microservices.Userservice;
 
+import com.deepak.microservices.Userservice.entities.Rating;
+import com.deepak.microservices.Userservice.external.services.RatingService;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class UserserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private RatingService ratingService;
+
+	@Test
+	void saveRating(){
+		Rating rating = Rating.builder()
+				.rating(9)
+				.userId("")
+				.hotelId("")
+				.feedback("This is created using FeignClient.")
+				.build();
+		ResponseEntity<Rating> savedRating = ratingService.saveRating(rating);
+
+		System.out.println("New rating created!!! :-- "+savedRating);
 	}
 
 }
