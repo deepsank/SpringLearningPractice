a9caedf98f82cb2148578b735e259f54dc564ff3
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2026-02-07 23:51:15 +0530

    Learnt and implemented Fault Tolerance through Resillience4j circuit breaker, and used the yml configs

--- Userservice/src/main/java/com/deepak/microservices/Userservice/controllers/UserController.java
+++ Userservice/src/main/java/com/deepak/microservices/Userservice/controllers/UserController.java
@@ -2,6 +2,10 @@ package com.deepak.microservices.Userservice.controllers;
 
 import com.deepak.microservices.Userservice.entities.User;
 import com.deepak.microservices.Userservice.services.UserService;
+import com.deepak.microservices.Userservice.services.impl.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +19,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
     //create
 
     @PostMapping
@@ -25,9 +30,23 @@ public class UserController {
 
     //Fetch particular user
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelBreakerFallback")
     public ResponseEntity<User> fetchUser(@PathVariable String userId){
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    // Creating fallback method for Cicuit breaker
+    public ResponseEntity<User> ratingHotelBreakerFallback(String userId, Exception ex){
+        logger.info("Fallback is executed as hotel or rating service is down : {}",ex.getMessage());
+         User user = User.builder()
+                .email("dummy@gmail.com")
+                .userId(userId)
+                .name("dummy")
+                .info("This is dummy response as one of the internal service is down.")
+                .build();
+
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     //Fetch all users
