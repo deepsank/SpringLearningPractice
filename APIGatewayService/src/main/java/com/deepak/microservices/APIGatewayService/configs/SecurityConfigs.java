f1c28c3fd966348581b9c20f2a4f8b699864d33a
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2026-02-12 02:35:52 +0530

    Learnt and integrated OAUTH2 with AUTH0 (OAUTH2) in the API Gateway service

--- APIGatewayService/src/main/java/com/deepak/microservices/APIGatewayService/configs/SecurityConfigs.java
+++ APIGatewayService/src/main/java/com/deepak/microservices/APIGatewayService/configs/SecurityConfigs.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,10 +18,14 @@ public class SecurityConfigs {
                 .authorizeExchange(exchanges -> exchanges
                         .anyExchange().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults())
+               .oauth2Login(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults())
                 )
+//               .oauth2Login(oauth2 -> oauth2
+//                       .authenticationSuccessHandler(
+//                        new RedirectServerAuthenticationSuccessHandler("/auth/login")
+//                ))
                 .build();
     }
 }
--- APIGatewayService/src/main/java/com/deepak/microservices/APIGatewayService/controllers/AuthController.java
+++ APIGatewayService/src/main/java/com/deepak/microservices/APIGatewayService/controllers/AuthController.java
@@ -6,10 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,23 +31,43 @@ public class AuthController {
 
     @GetMapping("/login")
     public ResponseEntity<AuthResponse> login(
-            @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient oAuth2AuthorizedClient,
+            @RegisteredOAuth2AuthorizedClient("auth0") OAuth2AuthorizedClient oAuth2AuthorizedClient,
             @AuthenticationPrincipal OidcUser oidcUser,
             Model model) {
         logger.info("User Email : {} ", oidcUser.getEmail());
+        System.out.println("User Email : {} "+ oidcUser.getEmail());
         AuthResponse authResponse = new AuthResponse();
         authResponse.setUserId(oidcUser.getEmail());
         authResponse.setAccessToken(oAuth2AuthorizedClient.getAccessToken().getTokenValue());
-        authResponse.setRefreshToken(oAuth2AuthorizedClient.getRefreshToken().getTokenValue());
-        authResponse.setExpiresIn(oAuth2AuthorizedClient.getAccessToken().getExpiresAt().getEpochSecond());
+        authResponse.setRefreshToken(oAuth2AuthorizedClient.getRefreshToken() != null ? oAuth2AuthorizedClient.getRefreshToken().getTokenValue() : null);
+        authResponse.setExpiresIn(oAuth2AuthorizedClient.getAccessToken().getExpiresAt() != null ? oAuth2AuthorizedClient.getAccessToken().getExpiresAt().getEpochSecond() : 0);
 
-         List<String> authorities = oidcUser.getAuthorities().stream().map(grantedAuthority -> {
-            return grantedAuthority.getAuthority();
-        }).toList();
+         List<String> authorities = oidcUser.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
 
          authResponse.setAuthorities(authorities);
 
          return new ResponseEntity<>(authResponse, HttpStatus.OK);
 
+    }
+
+    @GetMapping("/")
+    public String home(@AuthenticationPrincipal OidcUser user) {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+
+        return "Hello, " + user.getFullName() + "!<br/><br/>Authorities: " + authorities;
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('Administrator')")
+    public String admin(@AuthenticationPrincipal OidcUser user) {
+        return "Hello, Admin!<br/><br/><img src=" + user.getPicture() + " width=200/>";
+    }
+
+    @GetMapping("/profile")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
+    public Map<String, Object> profile(OAuth2AuthenticationToken authentication) {
+        return authentication.getPrincipal().getAttributes();
     }
 }
