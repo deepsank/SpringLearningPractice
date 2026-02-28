06f27b50f6a817f8b80cdd6cba28b734f5701e45
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2026-02-28 23:55:42 +0530

    Tried to create native image , and provide hints to application about the dynamic resources such as .pdf, .st, as Java needs to know beforehand about dunamic things due to its statically typed nature; using HintsRegistrar

--- SpringDocumentation/SpringDocumentation/src/main/java/com/springAI/SpringDocumentation/SpringDocumentationApplication.java
+++ SpringDocumentation/SpringDocumentation/src/main/java/com/springAI/SpringDocumentation/SpringDocumentationApplication.java
@@ -1,9 +1,13 @@
 package com.springAI.SpringDocumentation;
 
+import com.springAI.SpringDocumentation.configs.HintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.shell.command.annotation.CommandScan;
 
+
+@ImportRuntimeHints(HintsRegistrar.class)
 @CommandScan
 @SpringBootApplication
 public class SpringDocumentationApplication {
