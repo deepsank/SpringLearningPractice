71a8e03be99bd8fbf574ae415827d84d866d0329
Deepak Kumar Upadhyay <deepakkumarupadhyay63@gmail.com>
2026-03-18 23:21:49 +0530

    Implemented adding Conversational History in SpringAI using SpringAdvisors -- MessageChatMemoryAdvisor and in-memory chat context

--- chatbot/chatbot/src/main/java/com/springAI/chatbot/controller/ChatController.java
+++ chatbot/chatbot/src/main/java/com/springAI/chatbot/controller/ChatController.java
@@ -1,6 +1,10 @@
 package com.springAI.chatbot.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +16,15 @@ public class ChatController {
 
     private ChatClient chatClient;
     public ChatController(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(new InMemoryChatMemoryRepository())
+                .maxMessages(10)
+                .build();
+        this.chatClient = builder
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
+                .build();
     }
 
     @PostMapping("/chat")
